405b1cef38f165fb34d23f337e165a2e5f9d19aa
Jose Bovet Derpich <jose.bovet@gmail.com>
2022-07-13 16:19:37 -0400

    feat(terraform): add lke provider

---
+++ terraform/main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    linode = {
+      source  = "linode/linode"
+      version = "1.28.0"
+    }
+  }
+}
+provider "linode" {
+  token = var.token
+}
+
+resource "linode_lke_cluster" "awesome_cluster" {
+  k8s_version = var.k8s_version
+  label       = var.label
+  region      = var.region
+  tags        = var.tags
+
+  dynamic "pool" {
+    for_each = var.pools
+    content {
+      type  = pool.value["type"]
+      count = pool.value["count"]
+    }
+  }
+}
+
+resource "linode_nodebalancer" "awesome_cluster_lb" {
+  label                = var.label
+  region               = var.region
+  tags                 = var.tags
+  client_conn_throttle = var.conn_throttle
+}
+
+resource "linode_nodebalancer_config" "awesome_cluster_lb_config" {
+  nodebalancer_id = linode_nodebalancer.awesome_cluster_lb.id
+  port            = 443
+  protocol        = "http"
+  check           = "http"
+  check_path      = "/ping"
+  check_attempts  = 3
+  check_timeout   = 5
+  check_interval  = 30
+  algorithm       = "source"
+}
+
+resource "linode_nodebalancer_node" "awesome_cluster_lb_node" {
+  count           = var.nodes_count
+  nodebalancer_id = linode_nodebalancer.awesome_cluster_lb.id
+  config_id       = linode_nodebalancer_config.awesome_cluster_lb_config.id
+  address         = "${element(local.lke_node_ips, count.index)}:80"
+  label           = var.label
+  weight          = 50
+}
+
+//Data Sources
+data "linode_instances" "awesome_cluster" {
+  filter {
+    name   = "id"
+    values = local.lke_node_ids
+  }
+}
+
+locals {
+  lke_node_ids = flatten(
+    [for pool in linode_lke_cluster.awesome_cluster.pool :
+      [for node in pool.nodes : node.instance_id]
+    ]
+  )
+  lke_node_ips = data.linode_instances.awesome_cluster.instances.*.private_ip_address
+}
+
+//Export this cluster's attributes
+output "kubeconfig" {
+  value     = linode_lke_cluster.awesome_cluster.kubeconfig
+  sensitive = true
+}
+
+output "api_endpoints" {
+  value = linode_lke_cluster.awesome_cluster.api_endpoints
+}
+
+output "status" {
+  value = linode_lke_cluster.awesome_cluster.status
+}
+
+output "id" {
+  value = linode_lke_cluster.awesome_cluster.id
+}
+
+output "pool" {
+  value = linode_lke_cluster.awesome_cluster.pool
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,60 @@
+variable "token" {
+  description = "Your Linode API Access Token (required)"
+}
+
+variable "nodes_count" {
+  description = "Worker nodes count(required)"
+  default     = "3"
+}
+
+variable "k8s_version" {
+  description = "The Kubernetes version to use for this cluster. (required)"
+  default     = "1.23"
+}
+
+variable "label" {
+  description = "The unique label to assign to this cluster. (required)"
+  default     = "awesome_cluster"
+}
+
+variable "region" {
+  description = "The region where your cluster will be located. (required)"
+  default     = "us-east"
+}
+
+variable "tags" {
+  description = "Tags to apply to your cluster for organizational purposes. (optional)"
+  type        = list(string)
+  default     = ["awesome_cluster", "labs", "k8s"]
+}
+variable "conn_throttle" {
+  description = "Throttle connections per second (0-20). Set to 0 (default) to disable throttling (optional)"
+  default     = "20"
+}
+
+variable "pools" {
+  description = "The Node Pool specifications for the Kubernetes cluster. (required)"
+  type = list(object({
+    type  = string
+    count = number
+  }))
+  default = [
+    {
+      type  = "g6-standard-1"
+      count = 3
+    }
+  ]
+}
+
+variable "cloudflare_email" {
+  description = "The email associated with the account. (required)"
+  default     = "jose.bovet@gmail.com"
+}
+
+variable "cloudflare_api_key" {
+  description = "The Cloudflare API key. (required)"
+}
+
+variable "cloudflare_zone_id" {
+  description = "The DNS zone ID to add the record to. (required)"
+}
