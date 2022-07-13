05b3e6d36f05a26753947840250faf573081e671
Jose Bovet Derpich <jose.bovet@gmail.com>
2022-07-13 16:22:28 -0400

    chore(terraform): remove cloudflare config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -45,16 +45,3 @@ variable "pools" {
     }
   ]
 }
-
-variable "cloudflare_email" {
-  description = "The email associated with the account. (required)"
-  default     = "jose.bovet@gmail.com"
-}
-
-variable "cloudflare_api_key" {
-  description = "The Cloudflare API key. (required)"
-}
-
-variable "cloudflare_zone_id" {
-  description = "The DNS zone ID to add the record to. (required)"
-}
