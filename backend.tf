7fc5c8bcf3032d26b40bd3562efae9a2fe9ea1e1
Javier Vera - Harness <javier.vera@harness.io>
2025-07-14 15:34:49 -0600

    feat: change bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "harness-deluxe-terraform-states"
+    bucket  = "harness-deluxe-terraform-state"
     key     = "terraform-linode-lke/s3/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
